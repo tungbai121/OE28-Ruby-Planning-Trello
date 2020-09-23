d9e1107fc725207cc7c63592eb793c327199dbc7
hungphong0910 <cityzen0910@gmail.com>
2020-09-23 11:29:38 +0700

    createlabel

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,6 +1,11 @@
 class Label < ApplicationRecord
+  PERMIT_ATTRIBUTES = %i(content).freeze
+
   has_many :tag_labels, class_name: TagLabel.name,
     foreign_key: :label_id,
     dependent: :destroy
   has_many :join_tags, through: :tag_labels, source: :tag
+
+  validates :content, presence: true,
+    length: {maximum: Settings.validate.max_label_content}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
     resources :users, only: :show
     resources :boards
+    resources :labels, only: :create
   end
 end
